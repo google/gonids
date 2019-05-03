837af7ac45ff97cb000bc7c7d6658a7c82fa1b59
duane <howard.duane@gmail.com>
2019-05-02 21:41:53 -0700

    Fix a bug where dataPosition wasn't reset at the beginning of each call to ParseRule. Fix a couple of tests.

--- utils/file_tester.go
+++ utils/file_tester.go
@@ -18,34 +18,6 @@ func commentFail(r *gonids.Rule) bool {
 
 func main() {
 	test := "/home/duane/Downloads/rules/emerging-trojan.rules"
-	s1 := []byte(`Content-Disposition`)
-	var b1 byte = 0x3a
-	s2 := []byte(` form-data`)
-	var b2 byte = 0x3b
-	s3 := []byte(`name=`)
-	var b3 byte = 0x22
-	s4 := []byte(` file`)
-	b4 := []byte{0x22, 0x3b}
-	s5 := []byte(`filename=`)
-	var b5 byte = 0x22
-	s6 := []byte(`C`)
-	var b6 byte = 0x3a
-	s7 := []byte(`\`)
-	bslice := append(s1, b1)
-	bslice = append(bslice, s2...)
-	bslice = append(bslice, b2)
-	bslice = append(bslice, s3...)
-	bslice = append(bslice, b3)
-	bslice = append(bslice, s4...)
-	bslice = append(bslice, b4...)
-	bslice = append(bslice, s5...)
-	bslice = append(bslice, b5)
-	bslice = append(bslice, s6...)
-	bslice = append(bslice, b6)
-	bslice = append(bslice, s7...)
-
-	fmt.Println(fmt.Sprintf("% 02x ", bslice))
-	return
 	fmt.Println(fmt.Sprintf("reading from: %s", test))
 	file, err := os.Open(test)
 	if err != nil {

ce7dc06676844ff7ef71907948f9a925739aa904
duane <howard.duane@gmail.com>
2019-05-02 21:40:48 -0700

    Revert "Set dataPosition to pktData at the beginning of each call to ParseRule"
    This reverts commit 92b22c7d3f657b0e0131efe068b174b65b69d57e.

--- parser.go
+++ parser.go
@@ -391,7 +391,6 @@ func ParseRule(rule string) (*Rule, error) {
 	if err != nil {
 		return nil, err
 	}
-	dataPosition = pktData
 	r := &Rule{}
 	for item := l.nextItem(); item.typ != itemEOR && item.typ != itemEOF && err == nil; item = l.nextItem() {
 		switch item.typ {
